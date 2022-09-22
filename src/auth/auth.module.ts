55ec5b90dbd3acf8de741ace16788d33f1ac0018
Anita-XR-LIU <scorpioliu1108@gmail.com>
2022-09-22 18:25:38 +0930

    build: delete empty files and created HTTP requests in user controller

--- src/auth/auth.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './services/auth/auth.service';
-
-@Module({
-  imports: [],
-  providers: [AuthService],
-})
-export class AuthModule {}
