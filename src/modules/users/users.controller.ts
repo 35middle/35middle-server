514cb451acbec03ac3e21395ad69972cbe8bd60d
Lydia <mengyao1227@gmail.com>
2022-10-31 10:56:47 +1100

    feat(getme): implement getme

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,13 +1,44 @@
-import { Controller, Delete, Param, Put } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Headers,
+  Param,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { MongoIdParams } from '../../utils/MongoIdParams';
+import { JwtGuard } from '../../guards/jwt.guard';
+import { UserEntity } from './user.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller({
   version: '1',
   path: 'users',
 })
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+  @Get('/getme')
+  @UseGuards(JwtGuard)
+  async getme(
+    @Headers('authorization') bearerToken: string,
+  ): Promise<UserEntity> {
+    const jwt = bearerToken.replace('Bearer ', '');
+    try {
+      const userEmail = this.jwtService.decode(jwt, { json: true }) as {
+        email: string;
+      };
+      return await UserEntity.fromObject(
+        this.usersService.findByEmail(userEmail.email),
+      );
+    } catch (e) {
+      throw e;
+    }
+  }
 
   @Delete(':id')
   delete(@Param() { id }: MongoIdParams): string {
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -4,10 +4,16 @@ import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './users.schema';
 import { UsersRepo } from './users.repo';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from '../auth/constants';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '600000s' },
+    }),
   ],
   controllers: [UsersController],
   providers: [UsersService, UsersRepo],
