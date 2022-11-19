e7adc740e851f90d2e8fcab704a8c8f25611ddd1
Mitchell <mingchenothin9@gmail.com>
2022-11-19 15:38:35 +1100

    feat: save button and read button

--- src/modules/buttons/buttons.module.ts
+++ src/modules/buttons/buttons.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Button, ButtonSchema } from './buttons.schema';
+import { ButtonsController } from './buttons.controller';
+import { ButtonsService } from './buttons.service';
+import { ButtonsRepo } from './buttons.repo';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Button.name, schema: ButtonSchema }]),
   ],
-  controllers: [],
-  providers: [],
-  exports: [],
+  controllers: [ButtonsController],
+  providers: [ButtonsService, ButtonsRepo],
+  exports: [ButtonsService],
 })
 export class ButtonsModule {}
